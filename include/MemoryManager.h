1d5a232e66d9177930d3a295cc7a6fc8bbe557e5
Infant Savari <144463656+InfantSavari@users.noreply.github.com>
2025-03-26 11:19:07 +0530

    Add files via upload

--- include/MemoryManager.h
+++ include/MemoryManager.h
@@ -1,3 +1,6 @@
+#ifndef MEMORYMANAGER.H
+#define MEMORYMANAGER.h
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
@@ -31,99 +34,51 @@ struct ResourceRequest {
 class ResourceAllocationTable {
 private:
    
-    unordered_map<int, unordered_map<ResourceType, size_t>> allocated_resources;
-    
-    
-    unordered_map<ResourceType, size_t> total_resources;
-    
-   
-    vector<ResourceRequest> pending_requests;
-    
-    
+    unordered_map<int, unordered_map<ResourceType, size_t>> allocated_resources;    
+    unordered_map<ResourceType, size_t> total_resources;  
+    vector<ResourceRequest> pending_requests;    
     const chrono::seconds DEADLOCK_TIMEOUT{5};
 
 public:
-    ResourceAllocationTable() {
-        
+    ResourceAllocationTable() {        
         total_resources[ResourceType::MEMORY] = 1024 * 1024;
-        total_resources[ResourceType::DISK] = 10 * 1024 * 1024;
-        
+        total_resources[ResourceType::DISK] = 10 * 1024 * 1024;        
     }
 
-    
-    // bool request_resource_with_ignorance(int process_id, ResourceType type, size_t amount) {
-
-    //     if (total_resources[type] >= amount) {
-    //         allocated_resources[process_id][type] += amount;
-    //         total_resources[type] -= amount;
-            
-    //         cout << "Process " << process_id 
-    //                   << " allocated " << amount 
-    //                   << " of " << static_cast<int>(type) 
-    //                   << " without deadlock check\n";
-    //         return true;
-    //     }
-        
-        
-    //     cerr << "Resource allocation failed for Process " 
-    //               << process_id << endl;
-    //     return false;
-    // }
-
-    
-    bool request_resource_with_detection(int process_id, ResourceType type, size_t amount) {
-        
+    bool request_resource_with_detection(int process_id, ResourceType type, size_t amount) {        
         ResourceRequest request{
             process_id, 
             type, 
             amount, 
             chrono::steady_clock::now()
         };
-        pending_requests.push_back(request);
-
-        
-        if (detect_deadlock()) {
-        
+        pending_requests.push_back(request);        
+        if (detect_deadlock()) {        
             pending_requests.pop_back();
             cerr << "Potential deadlock detected. Request rejected.\n";
             return false;
-        }
-
-        
+        }        
         allocated_resources[process_id][type] += amount;
         total_resources[type] -= amount;
-        
-        cout << "Process " << process_id 
-                  << " allocated " << amount 
-                  << " of " << static_cast<int>(type) 
-                  << " after deadlock detection\n";
+        cout <<"Process "<<process_id<<" allocated "<<amount<<" of "<<static_cast<int>(type)<< " after deadlock detection\n";
         return true;
     }
 
-    
     bool detect_deadlock() {
-        auto now = chrono::steady_clock::now();
-        
+        auto now = chrono::steady_clock::now();        
         for (const auto& request : pending_requests) {
             if (now - request.timestamp > DEADLOCK_TIMEOUT) {
-
                 return true;
             }
-        }
-        
-        
+        }     
         return false;
     }
 
-
     bool request_resource_with_prevention(int process_id, ResourceType type, size_t amount) {
-        
-        if (allocated_resources[process_id][type] + amount > total_resources[type]) {
+       if (allocated_resources[process_id][type] + amount > total_resources[type]) {
             std::cerr << "Request would exceed maximum possible allocation\n";
             return false;
-        }
-
-        
+        }            
         bool can_allocate = true;
         for (const auto& [existing_type, existing_amount] : allocated_resources[process_id]) {
             if (static_cast<int>(existing_type) > static_cast<int>(type)) {
@@ -131,24 +86,15 @@ public:
                 break;
             }
         }
-
         if (!can_allocate) {
             cerr << "Resource allocation prevented to avoid potential deadlock\n";
             return false;
-        }
-
-       
+        }             
         allocated_resources[process_id][type] += amount;
-        total_resources[type] -= amount;
-        
-        cout << "Process " << process_id 
-                  << " allocated " << amount 
-                  << " of " << static_cast<int>(type) 
-                  << " with prevention\n";
+        total_resources[type] -= amount;        
+        cout << "Process "<<process_id<<" allocated "<<amount<<" of "<<static_cast<int>(type)<<" with prevention\n";
         return true;
     }
-
-
     void release_resources(int process_id) {
         for (auto& [type, amount] : allocated_resources[process_id]) {
             total_resources[type] += amount;
@@ -160,5 +106,6 @@ public:
     }
 };
 
+#endif
 
 
