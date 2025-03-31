33eca042b92f69102bcd8be48497cddeacf75c53
Infant Savari <144463656+InfantSavari@users.noreply.github.com>
2025-03-31 19:58:34 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Process.hpp"
 #include "Scheduler.hpp"
-#include "MemoryManager.hpp"
+#include "MemoryManager.h"
+#include "IPC.h"
 #include <iostream>
 #include <vector>
 
@@ -9,44 +10,52 @@ using namespace std;
 int main(){
     vector<Process> process;
     int pid,at,bt,priority;
-    ResourceAllocationTable program;
+    ResourceAllocationTable rat(5);
     int n1,n2,n3;
     int choice,p=0;
     IPC ipc;
-    cout<<"******************************************************"<<endl;
-    cout<<"                 OS Kernel Simulator                 \n\n"<<endl;
-    cout<<"******************************************************"<<endl;
+    cout<<"                 *********************************************************"<<endl;
+    cout<<"                                     OS Kernel Simulator                 \n\n"<<endl;
+    cout<<"                 *********************************************************"<<endl;
     while(true){
         
-        cout<<"1. Create Process \n2. Start Execution\n3. Exit"<<endl;
+        cout<<"                 1. Create Process \n
+                                2. Start Execution\n
+                                3. Exit"<<endl;
+        
         cin>>choice;
         if(choice == 3){
-            p=0
-            continue;
+            p=0;
+            process.clear();
+			continue;
         }
+        // ResourceAllocationTable rat;
         if(choice == 1){
-            cout<<"Existing process details : PID  |  Arrival Time  |  Burst Time | resources( A ) | resources( B ) | resources( C ) "<<endl;
-            for(auto &m : process){
-                cout<<" Process PID "<<m.pid<<" | "<<m.arrivalTime<<" | "<<m.execution_time<<" | ";
-                rat.display(m.pid);
-            }
-            cout<<endl;
-            cout<<"Enter details of process: \n"<<endl;
-            cout<<"     1. Enter PID: ";
-            cin>>pid;
-            cout<<"     2. Enter process "<<pid<<" arrival time : ";
-            cin>>at;
-            cout<<"     3. Enter process "<<pid<<" burst time : ";
-            cin>>bt;
-            cout<<"     4. Enter maximum resource  needed for process [ A, B, C ]"<<pid<<" : ";
-            cin>>n1>>n2>>n3;
-            int m1,m2,m3;
-            cout<<"     5. Enter request from process [ A, B, C ]"<<pid<<" : ";
-            cin>>m1>>m2>>m3;
-            Process temp(pid,at,bt);
-            temp.priority =p++;
-            rat.set_max_demand(pid,{n1,n2,n3});
-            rat.request_resources(pid,{m1,m2,m3});
+            cout<<"                 Enter number of process: ";
+            int n;
+            cin>>n;
+        	for(int i=0;i<n;i++){
+        		cout<<"                 Existing process details : PID  |  Arrival Time  |  Burst Time | resources( A ) | resources( B ) | resources( C ) "<<endl;
+            	rat.display_state();            	
+            	cout<<endl;
+            	cout<<"Enter details of process: \n"<<endl;
+            	cout<<"     1. Enter PID: ";
+            	cin>>pid;
+            	cout<<"     2. Enter process "<<pid<<" arrival time : ";
+            	cin>>at;
+            	cout<<"     3. Enter process "<<pid<<" burst time : ";
+            	cin>>bt;
+            	cout<<"     4. Enter maximum resource  needed for process [ A, B, C ]"<<pid<<" : ";
+            	cin>>n1>>n2>>n3;
+            	int m1,m2,m3;
+            	cout<<"     5. Enter request from process [ A, B, C ]"<<pid<<" : ";
+            	cin>>m1>>m2>>m3;
+            	Process temp(pid,at,bt);
+            	temp.priority =p++;
+            	rat.set_max_demand(pid,{n1,n2,n3});
+            	rat.request_resources(pid,{m1,m2,m3});
+			}
+            
             continue;
         }
         if(choice == 2){
@@ -55,7 +64,7 @@ int main(){
             cout<<"     5. Let kernel decide: "<<endl;
             cin>>choice;
             if(choice == 1){
-                fcfs(process, &rat);
+                fcfs(process, rat,ipc);
                 printResults(process);
                 continue;
             }
@@ -66,27 +75,27 @@ int main(){
                     cout<<"PID "<<p.pid<<" Priority: "<<endl;
                     cin>>p.priority;
                 }
-                priorityScheduling(process);
+                priorityScheduling(process,rat,ipc);
                 continue;
             }
             else if(choice == 3){
-                shortestJobFirst(process);
+                shortestJobFirst(process, rat,ipc);
                 continue;
             }
             else if (choice == 4){
                 int qt;
                 cout<<"Enter quantum time: ";
                 cin>>qt;
-                roundRobin(process,qt);
+                roundRobin(process,qt,rat,ipc);
                 printResults(process);
                 continue;
             }
             else{
-                MultiLevelQueueScheduler mqs;
-                for(auto &i : process){
-                    mqs.addProcess(i);
-                }
-                mqs.executeProcesses();
+                // MultiLevelQueueScheduler mqs;
+                // for(auto &i : process){
+                //     mqs.addProcess(i);
+                // }
+                // mqs.executeProcesses();
                 continue;
             }
         }
