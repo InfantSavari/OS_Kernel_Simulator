e7a76358ea89ffe23ad7fe2de039495beb9ee3d2
Infant Savari <144463656+InfantSavari@users.noreply.github.com>
2025-04-07 16:01:37 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "MemoryManager.h"
 #include "IPC.h"
 #include <iostream>
+// #include <windows.h>
 #include <vector>
 
 using namespace std;
@@ -10,37 +11,54 @@ using namespace std;
 int main(){
     vector<Process> process;
     int pid,at,bt,priority;
-    ResourceAllocationTable rat(5);
+    ResourceAllocationTable rat;
     int n1,n2,n3;
     int choice,p=0;
     IPC ipc;
-    cout<<"                 *********************************************************"<<endl;
-    cout<<"                                     OS Kernel Simulator                 \n\n"<<endl;
-    cout<<"                 *********************************************************"<<endl;
+    cout<<"|*********************************************************|"<<endl;
+    cout<<"|                     OS Kernel Simulator                 |"<<endl;
+    cout<<"|*********************************************************|"<<endl;
     while(true){
-        
-        cout<<"                 1. Create Process \n
-                                2. Start Execution\n
-                                3. Exit"<<endl;
-        
+        cout<<"|                                                         |"<<endl;
+        cout<<"|Main Menu:                                               |"<<endl;     
+        cout<<"|         1. Create Process                               |"<<endl;
+        cout<<"|         2. Start Execution                              |"<<endl;
+        cout<<"|         3. Exit                                         |"<<endl;
+        cout<<"|_________________________________________________________|"<<endl;
+        cout<<endl;
+        cout<<"Current state of System: "<<endl;
+        rat.display_state();
+        cout<<"|---------------------------------------------------------|"<<endl;
+        cout<<"Enter your choice: ";
         cin>>choice;
         if(choice == 3){
             p=0;
             process.clear();
+            cout << "\033[2J\033[H";
+            cout<<"|*********************************************************|"<<endl;
+            cout<<"|                     OS Kernel Simulator                 |"<<endl;
+            cout<<"|*********************************************************|"<<endl; 
 			continue;
         }
         // ResourceAllocationTable rat;
         if(choice == 1){
-            cout<<"                 Enter number of process: ";
+            cout<<"Enter number of process: ";
             int n;
             cin>>n;
         	for(int i=0;i<n;i++){
-        		cout<<"                 Existing process details : PID  |  Arrival Time  |  Burst Time | resources( A ) | resources( B ) | resources( C ) "<<endl;
-            	rat.display_state();            	
+        		cout<<"Existing process details : "<<endl;
+                               
+                rat.display_state();            	
             	cout<<endl;
             	cout<<"Enter details of process: \n"<<endl;
             	cout<<"     1. Enter PID: ";
             	cin>>pid;
+                for(auto& p : process){
+                    if(p.pid == pid){
+                        cout<<"Process with PID "<<pid<<" already exists!!!"<<endl;
+                        continue;
+                    }
+                }
             	cout<<"     2. Enter process "<<pid<<" arrival time : ";
             	cin>>at;
             	cout<<"     3. Enter process "<<pid<<" burst time : ";
@@ -48,25 +66,32 @@ int main(){
             	cout<<"     4. Enter maximum resource  needed for process [ A, B, C ]"<<pid<<" : ";
             	cin>>n1>>n2>>n3;
             	int m1,m2,m3;
+                vector<int> max = {n1,n2,n3};
             	cout<<"     5. Enter request from process [ A, B, C ]"<<pid<<" : ";
             	cin>>m1>>m2>>m3;
+                vector<int> req = {m1,m2,m3};
             	Process temp(pid,at,bt);
-            	temp.priority =p++;
-            	rat.set_max_demand(pid,{n1,n2,n3});
-            	rat.request_resources(pid,{m1,m2,m3});
+            	
+            	rat.set_max_demand(pid,max);
+            	bool t = rat.request_resources(pid,req);
+                if(!t){
+                    cout<<"Process "<<pid<<" is discarded!!!"<<endl;
+                    continue;
+                }
+            	process.push_back(temp);
 			}
-            
+            cout<<"_________________________________________________________"<<endl;
             continue;
         }
         if(choice == 2){
             cout<<"Process scheduling algorithm: "<<endl;
             cout<<"     1. First Come First Serve\n     2. Priority Scheduling\n        3. ShortestRemainingJobFirst\n      4.roundRobin"<<endl;
-            cout<<"     5. Let kernel decide: "<<endl;
+            // cout<<"     5. Let kernel decide: "<<endl;
             cin>>choice;
             if(choice == 1){
                 fcfs(process, rat,ipc);
                 printResults(process);
-                continue;
+                
             }
             else if(choice == 2){
                 
@@ -76,11 +101,11 @@ int main(){
                     cin>>p.priority;
                 }
                 priorityScheduling(process,rat,ipc);
-                continue;
+                
             }
             else if(choice == 3){
                 shortestJobFirst(process, rat,ipc);
-                continue;
+                
             }
             else if (choice == 4){
                 int qt;
@@ -88,16 +113,10 @@ int main(){
                 cin>>qt;
                 roundRobin(process,qt,rat,ipc);
                 printResults(process);
-                continue;
+                
             }
-            else{
-                // MultiLevelQueueScheduler mqs;
-                // for(auto &i : process){
-                //     mqs.addProcess(i);
-                // }
-                // mqs.executeProcesses();
-                continue;
-            }
+            
+            cout<<"_________________________________________________________"<<endl;
         }
 
     }
